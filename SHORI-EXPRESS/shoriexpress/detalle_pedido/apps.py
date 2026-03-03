c624f100e3b875a4258198da2210b621db5ea419
adrian083 <adrianmauricio567@gmail.com>
2026-03-02 21:53:12 -0500

    subida inicial del proyecto Shori Express
    aqui estoy subiendo toda la base del proyecto para empezar a trabajar en serio desde el repo. Ya inclui las carpetas principales, la configuracion del server y los assets basicos. Falta pulir unas rutas pero la estructura ya esta lista para que todos metamos mano.

---
+++ SHORI-EXPRESS/shoriexpress/detalle_pedido/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DetallePedidoConfig(AppConfig):
+    name = 'detalle_pedido'
---
+++ SHORI-EXPRESS/shoriexpress/metodo_pago/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MetodoPagoConfig(AppConfig):
+    name = 'metodo_pago'
